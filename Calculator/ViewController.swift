bf2976c21254c32a6ba6d6088ac5fc68db82663f
Mamdouh <Mamdohreda@gmail.com>
2022-11-01 22:32:43 +0000

    number button function and top cal button works too

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,19 +11,58 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
+    private var finishTypeNumber: Bool = true
     
     
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        finishTypeNumber = true
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("there error here")
+        }
+        
+        if let calMethod = sender.currentTitle{
+            if calMethod == "+/-"{
+                displayLabel.text = String(number * -1)
+            }else if calMethod == "AC"{
+                displayLabel.text = "0"
+            }else if calMethod == "%"{
+                displayLabel.text = String(number / 100)
+            }
+        }
+        
+        
+        
     }
 
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
-        
         //What should happen when a number is entered into the keypad
+        if let numValue = sender.currentTitle {
+            if finishTypeNumber == true{
+                displayLabel.text = numValue
+                finishTypeNumber = false
+            }else{
+                if numValue == "."{
+                guard let currentDisplayValue = Double(displayLabel.text!)else
+                {
+                    fatalError("error here")
+                }
+                let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    if !isInt{
+                        return
+                    }
+                    
+            }
+                
+                
+                displayLabel.text! += numValue
+            }
+        }
+        
+        
     
     }
 
